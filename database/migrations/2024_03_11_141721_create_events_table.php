5723e5cae4f237fb6aafb7d208ef273e7aa931db
AhmadOthman <trollingosman101@outlook.com>
2024-03-16 13:32:15 +0200

    update 5 files

--- database/migrations/2024_03_11_141721_create_events_table.php
+++ database/migrations/2024_03_11_141721_create_events_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string("name");
-            $table->enum("type",["individual","team"])->default("individual");
+            $table->enum("type", ["individual", "team"])
+                ->default("individual");
             $table->timestamps();
         });
     }
@@ -22,8 +21,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('events');
     }
 };
